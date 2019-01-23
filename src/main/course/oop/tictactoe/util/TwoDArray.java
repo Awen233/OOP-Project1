13f4a7dcfcaaf001d4a44abd4cd51e1ad9797ed3
Awen233 <dingzhaowen1997@gmail.com>
2019-01-22 22:29:20 -0500

    project one complete

--- src/main/course/oop/tictactoe/util/TwoDArray.java
+++ src/main/course/oop/tictactoe/util/TwoDArray.java
@@ -1,5 +1,5 @@
 package main.course.oop.tictactoe.util;
-
+import java.util.*;
 /**
  * You must implement the following methods to accept the expected 
  * parameters and return accurate results.
@@ -10,18 +10,29 @@ package main.course.oop.tictactoe.util;
  */
 public class TwoDArray {
 	
+	int[][] array;
+	Map<Integer, Integer> map; 
+	int defaultVal;
 	
 	public TwoDArray(int rows, int cols, int defaultVal){
 		/*TODO - Create a 2D integer array consisting of 
 		 * the number of rows and columns given. Initialize 
 		 * the array by setting each int to be the defaulVal. 
 		 * */
+		array = new int[rows][cols];
+		this.defaultVal = defaultVal;
+		map = new HashMap<>(); 
+		fill(array, defaultVal);
+		
 	}
 	
 	public void initArray(int defaultVal) {
 		/*TODO - (Re)Initialize the array by 
 		 * setting each int to be the defaulVal 
 		 */
+		map.clear();
+		fill(array, defaultVal);
+		this.defaultVal = defaultVal;
 		
 	}
 	
@@ -39,15 +50,23 @@ public class TwoDArray {
 		 * Note: Print the int value in place of (). 
 		 * e.g., replace (val) with val.
 		 */
-		return "Not implemented";
+		if(val == defaultVal) {
+			return "Failure: " + val + " is not allowed";
+		} else if ( array[row][col] != defaultVal) {
+			return "Failure: " + row + ", " + col + " is not empty";
+		} else {
+			array[row][col] = val;
+		}
+		int count = map.getOrDefault(val, 0);
+		map.put(val, count + 1);
+		return "Success! " + val + " was inserted";
 	}
 	
 	public int getInt(int row, int col) {
 		/*TODO - Return the value at the specified row, col
 		 * 
 		 */
-		
-		return 0;
+		return array[row][col];
 	}
 	
 	public String getArrayDisplay() {
@@ -58,8 +77,16 @@ public class TwoDArray {
 		 *  0	1	1
 		 * 
 		 */
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[0].length; j++) {
+				sb.append(array[i][j]);
+				sb.append("\t");
+			}
+			sb.append("\n");
+		}
 		
-		return "Not implemented";
+		return sb.toString();
 	}
 	
 	public String getArrayDetails() {
@@ -74,8 +101,27 @@ public class TwoDArray {
 		 * 			)
 		 * 
 		 */
-		
-		return "Not implemented";
+		StringBuilder sb = new StringBuilder();
+		sb.append("# ");
+		sb.append(array.length);
+		sb.append("\n");
+		sb.append("# " + array[0].length + "\n");
+		sb.append("Value and count of each ( \n");
+		for(Integer k : map.keySet()) {
+			sb.append("\t \t value:" + k + " count:" + map.get(k) + "\n");
+		}
+		sb.append("\n \t \t");
+		return sb.toString();
 	}		
-
+	
+	private void fill(int[][] array, int val) {
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[0].length; j++) {
+				array[i][j] = val;
+				int count = map.getOrDefault(val, 0);
+				map.put(val, count + 1);
+			}
+		}
+	}
+	
 }
